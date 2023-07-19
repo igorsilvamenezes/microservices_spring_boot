2af44a37addc50ff8219614bd36ca39ae8f2dcbc
Igor Menezes <igorsilvamenezes@gmail.com>
2023-07-19 16:00:56 -0300

    Integration between Customer and Fraud module

--- Fraud/src/main/java/com/menezes/fraud/FraudController.java
+++ Fraud/src/main/java/com/menezes/fraud/FraudController.java
@@ -1,11 +1,13 @@
 package com.menezes.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -17,6 +19,8 @@ public class FraudController {
     public FraudCheckResponse isFraudster(
             @PathVariable Integer customerId ) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudlentCustomer(customerId);
+        log.info("fraud check response for customer {}", customerId);
+
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
